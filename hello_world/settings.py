7ed224d624851dc7877477128f03985e97fdc1f8
Kelmy A Lemus <klemus1@samford.edu>
2026-04-15 14:57:41 +0000

    modified hello_world file and requirements file

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -86,13 +86,23 @@ WSGI_APPLICATION = "hello_world.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": dj_database_url.parse(
-        config("DATABASE_URL", default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"),
-        conn_max_age=600,
-        ssl_require=False,
-    )
-}
+database_url = config("DATABASE_URL", default="")
+
+if database_url:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            database_url,
+            conn_max_age=600,
+            ssl_require=False,
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
